1628c91efca6a7c6555943d4ce183bfe2591ff1e
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-08-25 21:01:53 +0530

    azure resources

--- security-group/main.tf
+++ security-group/main.tf
@@ -66,3 +66,42 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 }
+resource "azurerm_network_interface" "nic" {
+  name                = "alma-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "almalinux-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2ats_v2"
+
+  admin_username = "sandeep"
+  admin_password = "Sandeep.,@0088"
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "almalinux"
+    offer     = "almalinux-x86_64"
+    sku       = "9-gen2"
+    version   = "latest"
+  }
+}
