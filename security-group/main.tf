caa1c33c993ce2aacb8f7318c73b78a7cfe1582f
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-08-26 22:44:40 +0530

    terraform null resource

--- security-group/main.tf
+++ security-group/main.tf
@@ -152,7 +152,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo dnf install ansible-core npm unzip -y",
-      "ansible-pull -i '${each.key}-dev.sandeepkumar5427.online', -U https://github.com/Sandeepkumar0088/roboshop-ansible-templates.git main.yml -e component=${each.key} -e env=dev"
+      "ansible-pull -i localhost, -U https://github.com/Sandeepkumar0088/roboshop-ansible-templates.git main.yml -e component=${each.key} -e env=dev"
     ]
 
   }
