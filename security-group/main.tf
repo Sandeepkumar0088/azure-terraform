3766784b2fdf0e365c3f7f7d2aaa725e7949e156
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-08-30 20:29:03 +0530

    inbound 8080 port

--- security-group/main.tf
+++ security-group/main.tf
@@ -66,6 +66,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "Allow-8080"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 resource "azurerm_network_interface" "nic" {
 
