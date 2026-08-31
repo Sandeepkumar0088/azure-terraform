c9e76df8d408c92d756d91ff6ac2eb83c8886134
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-08-31 14:58:30 +0530

    terraform null resource

--- security-group/main.tf
+++ security-group/main.tf
@@ -162,12 +162,12 @@ resource "null_resource" "ansible" {
       host      = azurerm_linux_virtual_machine.vm[each.key].public_ip_address
     }
 
-    # inline = [
-    #   "echo 'sandeep' > vault-pass.txt",
-    #   "chmod 600 vault-pass.txt",
-    #   "sudo dnf install ansible-core npm unzip -y",
-    #   "ansible-pull -i localhost, -U https://github.com/Sandeepkumar0088/roboshop-ansible-templates.git main.yml -e component=${each.key} -e env=dev --vault-password-file vault-pass.txt"
-    # ]
+    inline = [
+      "echo 'sandeep' > vault-pass.txt",
+      "chmod 600 vault-pass.txt",
+      "sudo dnf install ansible-core npm unzip -y",
+      "ansible-pull -i localhost, -U https://github.com/Sandeepkumar0088/roboshop-ansible-templates.git main.yml -e component=${each.key} -e env=dev --vault-password-file vault-pass.txt"
+    ]
 
   }
 }
