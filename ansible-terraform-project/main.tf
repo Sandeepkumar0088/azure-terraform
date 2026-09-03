aabff13edc8d7858a4029c9afa9e540408d84908
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-09-03 15:14:58 +0530

    azure ansible + terraform project

--- ansible-terraform-project/main.tf
+++ ansible-terraform-project/main.tf
@@ -187,7 +187,7 @@ resource "null_resource" "ansible" {
       "echo 'sandeep' > ~/vault-pass.txt",
       "chmod 600 ~/vault-pass.txt",
       "sudo dnf install -y ansible-core git",
-      "ansible-pull -U https://github.com/Sandeepkumar0088/azure-ansible.git main.yml -e component=${each.key} -e env=dev --vault-password-file ~/vault-pass.txt"
+      "ansible-pull -i localhost, -U https://github.com/Sandeepkumar0088/azure-ansible.git main.yml -e component=${each.key} -e env=dev --vault-password-file ~/vault-pass.txt"
     ]
   }
 }
