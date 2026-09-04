77e16ed8e1f94ea2cdca7b093dff2c77cbb8af35
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-09-04 15:39:33 +0530

    azure ansible + terraform project

--- ansible-terraform-project/main.tf
+++ ansible-terraform-project/main.tf
@@ -85,17 +85,17 @@ resource "azurerm_network_security_group" "nsg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  security_rule {
-    name                       = "Allow-SSH"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "0.0.0.0/0"
-    destination_address_prefix = "*"
-  }
+  # security_rule {
+  #   name                       = "Allow-SSH"
+  #   priority                   = 100
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "22"
+  #   source_address_prefix      = "0.0.0.0/0"
+  #   destination_address_prefix = "*"
+  # }
 
   # Allow ALL inbound traffic
   security_rule {
