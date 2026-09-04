ff23f0c959546e91ae7f04042f05f3b2a4a0963d
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-09-04 17:06:52 +0530

    terraform null resource

--- ansible-terraform-project/work.tf
+++ ansible-terraform-project/work.tf
@@ -18,6 +18,11 @@ resource "azurerm_network_interface" "virtual_work" {
     public_ip_address_id          = azurerm_public_ip.virtual_work.id
   }
 }
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+
+  network_interface_id      = azurerm_network_interface.virtual_work.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
 resource "azurerm_linux_virtual_machine" "work" {
 
