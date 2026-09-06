87f47ea25f41e1af5b97e354de61663ab06ba851
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-09-06 13:21:32 +0530

    azure ansible + terraform project

--- ansible-terraform-project/main.tf
+++ ansible-terraform-project/main.tf
@@ -116,7 +116,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = each.key
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2ats_v2"
+  size                = each.value
 
   admin_username = "sandeep"
   admin_password = "Sandeep.,@0088"
