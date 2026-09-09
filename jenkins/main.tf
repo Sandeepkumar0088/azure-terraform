801f408991510d12307aae0d4f4c261ae918e885
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-09-09 21:03:04 +0530

    jenkins setup

---
+++ jenkins/main.tf
@@ -0,0 +1,136 @@
+resource "azurerm_resource_group" "cluster" {
+  name = "cluster"
+  location = "Central India"
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "cluster"
+  location            = azurerm_resource_group.cluster.location
+  resource_group_name = azurerm_resource_group.cluster.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "cluster"
+  resource_group_name  = azurerm_resource_group.cluster.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_public_ip" "jenkins" {
+  name                = "jenkins"
+  location            = azurerm_resource_group.cluster.location
+  resource_group_name = azurerm_resource_group.cluster.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "jenkins" {
+  name                = "jenkins"
+  location            = azurerm_resource_group.cluster.location
+  resource_group_name = azurerm_resource_group.cluster.name
+
+  # Allow ALL inbound traffic
+  security_rule {
+    name                       = "Allow-All-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+
+    source_port_range          = "*"
+    destination_port_range     = "*"
+
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+
+  # Allow ALL outbound traffic
+  security_rule {
+    name                       = "Allow-All-Outbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "jenkins" {
+
+  name                = "jenkins"
+  location            = azurerm_resource_group.cluster.location
+  resource_group_name = azurerm_resource_group.cluster.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.aks.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.jenkins.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "jenkins" {
+
+  network_interface_id      = azurerm_network_interface.jenkins.id
+  network_security_group_id = azurerm_network_security_group.jenkins.id
+}
+
+resource "azurerm_linux_virtual_machine" "jenkins" {
+  name                = "jenkins"
+  resource_group_name = azurerm_resource_group.cluster.name
+  location            = azurerm_resource_group.cluster.location
+  size                = "Standard_D4ls_v6"
+
+  admin_username                  = "sandeep"
+  admin_password                  = "Sandeep.,@0088"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.jenkins.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "almalinux"
+    offer     = "almalinux-x86_64"
+    sku       = "9-gen2"
+    version   = "latest"
+  }
+}
+
+resource "null_resource" "jenkins" {
+
+  depends_on = [
+    azurerm_linux_virtual_machine.jenkins
+  ]
+
+  connection {
+    type     = "ssh"
+    host     = azurerm_linux_virtual_machine.jenkins.public_ip_address
+    user     = "sandeep"
+    password = "Sandeep.,@0088"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf update -y",
+      "sudo dnf install git java-21-openjdk -y",
+      "sudo curl -L -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/rpm-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/rpm-stable/jenkins.io-2026.key",
+      "sudo yum install jenkins -y",
+      "sudo systemctl enable jenkins",
+      "sudo systemctl start jenkins",
+      "sudo dnf install -y dnf-plugins-core",
+      "sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
+      "sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+      "sudo systemctl enable docker",
+      "sudo systemctl start docker",
+      "sudo usermod -aG docker jenkins"
+    ]
+  }
+}
